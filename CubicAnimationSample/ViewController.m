8f83faeb89a1046006b5d5c608cc48fb71588fb2
chengkaizone <chengkaizone@163.com>
2015-10-13 23:48:08 +0800

    init

--- CubicAnimationSample/ViewController.m
+++ CubicAnimationSample/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "easing.h"
+#import "YXEasing.h"
 
 @interface ViewController ()
 
@@ -16,12 +18,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *backView = [[UIView alloc] initWithFrame:self.view.bounds];
+    backView.backgroundColor = [UIColor blackColor];
+    backView.alpha = 0;
+    [self.view addSubview:backView];
+    [UIView animateWithDuration:1.f animations:^{
+        backView.alpha = 0.3f;
+    }];
+    
+    
+    //创建模拟菜单
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    imageView.image = [UIImage imageNamed:@"pic"];
+    [self.view addSubview:imageView];
+    
+    CAKeyframeAnimation *keyFrameAnimation = [CAKeyframeAnimation animation];
+    keyFrameAnimation.keyPath = @"position";
+    keyFrameAnimation.duration = 1.f;
+    keyFrameAnimation.values = \
+        [YXEasing calculateFrameFromPoint:imageView.center
+                                  toPoint:CGPointMake(self.view.center.x + 100, self.view.center.y)
+                                     func:CubicEaseOut
+                               frameCount:1 * 30];
+    imageView.center = CGPointMake(self.view.center.x + 100, self.view.center.y);
+    [imageView.layer addAnimation:keyFrameAnimation forKey:nil];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
